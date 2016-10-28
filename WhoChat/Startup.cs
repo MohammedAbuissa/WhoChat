280e8961dd77be80dddec7c6546af795320091c8
moham <moham@DESKTOP-2F88OBO>
2016-10-28 15:03:50 +0200

    Add project files.

---
+++ WhoChat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WhoChat.Startup))]
+namespace WhoChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
